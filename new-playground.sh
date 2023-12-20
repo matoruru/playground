ab9082cc1dd3431997c1449ccd3f71f923807273
matoruru <matoruru@shgt.jp>
2023-12-20 12:26:14 +0900

    add: Init script

---
+++ new-playground.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu
+
+playgroundName=
+prefix='playground-'
+
+while [[ -z "$playgroundName" ]]; do
+  read -p "Name of the new playground?: (descriptive one is better) " playgroundName
+done
+
+folderName="$prefix$playgroundName"
+
+echo Creating...
+mkdir $folderName
+echo "# $folderName" > $folderName/README.md
+echo ✨ New playground \"$folderName\" has been created!
+ls -la $folderName
+
