9672c3223b1d2bea2d10102db4a9ed74fc39061d
matoruru <matoruru@shgt.jp>
2024-08-25 23:49:56 +0900

    wip

--- playgrounds/0012/go.mod
+++ playgrounds/0012/go.mod
@@ -1,3 +1,5 @@
 module example/hello
 
 go 1.22.5
+
+require golang.org/x/tour v0.1.0
--- playgrounds/0012/hello.go
+++ playgrounds/0012/hello.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+	"sync"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	start := time.Now()
+	post := fetchPost()
+
+	resChan := make(chan any, 2)
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go fetchPostLikes(post, resChan, &wg)
+	go fetchPostComments(post, resChan, &wg)
+
+	wg.Wait()
+
+	close(resChan)
+
+	for res := range resChan {
+		fmt.Println("res: ", res)
+	}
+
+	fmt.Println("took: ", time.Since(start))
+}
+
+func fetchPost() string {
+	time.Sleep(time.Millisecond * 50)
+	return "What programming language do you perefer?"
+}
+
+func fetchPostLikes(post string, resChan chan any, wg *sync.WaitGroup) {
+	time.Sleep(time.Millisecond * 50)
+    resChan <- 10
+	wg.Done()
+}
+
+func fetchPostComments(post string, resChan chan any, wg *sync.WaitGroup) {
+	time.Sleep(time.Millisecond * 100)
+	resChan <- []string{"Golang", "Java", "Rust"}
+	wg.Done()
 }
