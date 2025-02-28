8d1abc1a12b0eb787e7bb25e3cfd3d62dd070d29
matoruru <matoruru@shgt.jp>
2025-02-28 23:45:14 +0900

    add: function.json

--- playgrounds/0013/function_app.py
+++ playgrounds/0013/function_app.py
@@ -2,6 +2,8 @@ import logging
 
 import azure.functions as func
 
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
 @app.route(route="http_trigger1", auth_level=func.AuthLevel.FUNCTION)
 def http_trigger1(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
